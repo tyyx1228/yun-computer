71222d18b03f3446efe44b54a0edd078207bef42
codingme <347616185@qq.com>
2025-11-08 16:19:30 +0800

    Delete .github/workflows directory

--- .github/workflows/scripts/win-verify-rdp.ps1
+++
@@ -1,5 +0,0 @@
-# Verify RDP port 3389
-Write-Host "Tailscale IP: $env:TAILSCALE_IP"
-$testResult = Test-NetConnection -ComputerName $env:TAILSCALE_IP -Port 3389
-if (-not $testResult.TcpTestSucceeded) { throw "TCP connection to 3389 failed" }
-Write-Host "TCP connectivity successful!"
